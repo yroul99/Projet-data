6a9609fe44ef27979d092c3507ccaccd0040f55b
yroul99 <yoan.roul@edu.esiee.fr>
2025-11-02 02:47:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-# main.py
 from dash import Dash
 from src.pages.home import layout, register_callbacks
 
 app = Dash(__name__)
 app.title = "NBA 2021-22 — Avantage du terrain"
 
-# NE PAS appeler layout() : c'est un objet, pas une fonction
+# IMPORTANT : layout est un objet html.Div (pas une fonction)
 app.layout = layout
 
-# callbacks
 register_callbacks(app)
 
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=8050, debug=False)
+    app.run_server(host="0.0.0.0", port=8050, debug=True)  # debug=True pour l’auto-reload
