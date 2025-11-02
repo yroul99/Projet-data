7dc47539cd1a9cf54567a4d8ffab6a71a6bb3db3
yroul99 <yoan.roul@edu.esiee.fr>
2025-11-02 02:33:21 -0400

    Update: clean_data (altitude/B2B/Elo) + pages/home

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 from dash import Dash
+=======
+﻿from dash import Dash
+>>>>>>> 003b078 (Update: clean_data (altitude/B2B/Elo) + pages/home)
 from src.pages.home import layout, register_callbacks
 
 app = Dash(__name__)
@@ -8,4 +12,8 @@ app.layout = layout
 register_callbacks(app)
 
 if __name__ == "__main__":
+<<<<<<< HEAD
     app.run(host="0.0.0.0", port=8050, debug=True)  # ← nouveau: app.run(...)
+=======
+    app.run(host="0.0.0.0", port=8050, debug=True)
+>>>>>>> 003b078 (Update: clean_data (altitude/B2B/Elo) + pages/home)
