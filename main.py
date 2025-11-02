344d59ebae07179cbf781dc08ff5add890361383
yroul99 <yoan.roul@edu.esiee.fr>
2025-11-02 06:52:45 -0400

    Update: clean_data (altitude/B2B/Elo) + pages/home

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-from dash import Dash
-=======
-﻿from dash import Dash
->>>>>>> 003b078 (Update: clean_data (altitude/B2B/Elo) + pages/home)
+# main.py
+from dash import Dash, html
 from src.pages.home import layout, register_callbacks
 
 app = Dash(__name__)
-app.title = "NBA 2021-22 — Avantage du terrain"
+app.layout = html.Div([layout])  # <-- 'layout' est un composant, on ne l'appelle pas
 
-app.layout = layout
 register_callbacks(app)
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    app.run(host="0.0.0.0", port=8050, debug=True)  # ← nouveau: app.run(...)
-=======
     app.run(host="0.0.0.0", port=8050, debug=True)
->>>>>>> 003b078 (Update: clean_data (altitude/B2B/Elo) + pages/home)
